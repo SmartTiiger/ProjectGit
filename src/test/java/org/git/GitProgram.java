46bc8ca1aa58fc633fa0009b601509aadc46193f
SmartTiger <smadhubalan2017@gmail.com>
2022-05-03 17:36:51 +0530

    GitProgram

---
+++ src/test/java/org/git/GitProgram.java
@@ -0,0 +1,33 @@
+package org.git;
+
+public class GitProgram {
+
+	private void launch() {
+
+		System.out.println("chrome");
+	}
+
+	private void start() {
+
+		System.out.println("browser");
+
+	}
+
+	private void label() {
+
+		System.out.println("facebook");
+
+	}
+
+	private void soBad() {
+
+		System.out.println("Eclipse");
+
+	}
+
+	private void temperature() {
+
+		System.out.println("something");
+
+	}
+}
